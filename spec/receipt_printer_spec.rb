6051c2b965d9112afbc0bd4a1e98e6c9d2d794d7
Dmitry Maksyoma <ledestin@gmail.com>
2014-08-13 00:43:30 +1200

    * Added outside-in test and an executable.

---
+++ spec/receipt_printer_spec.rb
@@ -0,0 +1,23 @@
+require 'open3'
+
+describe 'receipt_printer' do
+  it 'given products, prints receipt' do
+    INPUT = <<EOF
+Quantity, Product, Price
+1, book, 12.49
+1, music cd, 14.99
+1, chocolate bar, 0.85
+EOF
+    EXPECTED_OUTPUT = <<EOF
+1, book, 12.49
+1, music CD, 16.49
+1, chocolate bar, 0.85
+
+Sales Taxes: 1.50
+Total: 29.83
+EOF
+
+    output, = Open3.capture2e('./receipt_printer', INPUT)
+    expect(output).to eq EXPECTED_OUTPUT
+  end
+end
