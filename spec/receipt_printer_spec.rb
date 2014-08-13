bc78a3bfa7a34a6f3e3cc83b4ef07ad1bc1f6465
Dmitry Maksyoma <ledestin@gmail.com>
2014-08-14 01:53:07 +1200

    * Refactor tests.

--- spec/receipt_printer_spec.rb
+++ spec/receipt_printer_spec.rb
@@ -7,13 +7,13 @@ end
 
 describe "receipt_printer, given products, prints receipt" do
   it "input 1" do
-    INPUT = <<EOF
+    input = <<EOF
 Quantity, Product, Price
 1, book, 12.49
 1, music cd, 14.99
 1, chocolate bar, 0.85
 EOF
-    EXPECTED_OUTPUT = <<EOF
+    expected_output = <<EOF
 1, book, 12.49
 1, music cd, 16.49
 1, chocolate bar, 0.85
@@ -21,34 +21,34 @@ EOF
 Sales Taxes: 1.50
 Total: 29.83
 EOF
-    run(INPUT, EXPECTED_OUTPUT)
+    run(input, expected_output)
   end
 
   it "input 2" do
-    INPUT = <<EOF
+    input = <<EOF
 Quantity, Product, Price
 1, imported box of chocolates, 10.00
 1, imported bottle of perfume, 47.50
 EOF
-    EXPECTED_OUTPUT = <<EOF
+    expected_output = <<EOF
 1, imported box of chocolates, 10.50
 1, imported bottle of perfume, 54.65
 
 Sales Taxes: 7.65
 Total: 65.15
 EOF
-    run(INPUT, EXPECTED_OUTPUT)
+    run(input, expected_output)
   end
 
   it "input 3" do
-    INPUT = <<EOF
+    input = <<EOF
 Quantity, Product, Price
 1, imported bottle of perfume, 27.99
 1, bottle of perfume, 18.99
 1, packet of headache pills, 9.75
 1, imported box of chocolates, 11.25
 EOF
-    EXPECTED_OUTPUT = <<EOF
+    expected_output = <<EOF
 1, imported bottle of perfume, 32.19
 1, bottle of perfume, 20.89
 1, packet of headache pills, 9.75
@@ -57,6 +57,6 @@ EOF
 Sales Taxes: 6.70
 Total: 74.68
 EOF
-    run(INPUT, EXPECTED_OUTPUT)
+    run(input, expected_output)
   end
 end
