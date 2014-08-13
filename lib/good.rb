5cf550fc22b68f72c70f245b760353edb7d35f24
Dmitry Maksyoma <ledestin@gmail.com>
2014-08-13 19:53:23 +1200

    * Refactor.

--- lib/good.rb
+++ lib/good.rb
@@ -4,13 +4,13 @@ class Good
   def self.parse_all str
     CSV.parse(str, :converters => :numeric).map! { |row|
       row.each { |el| el.strip! if el.is_a?(String) }
-      Good.new row
+      Good.new *row
     }
   end
 
   attr_reader :quantity, :name, :price
 
-  def initialize ar
-    @quantity, @name, @price = *ar
+  def initialize quantity, name, price
+    @quantity, @name, @price = quantity, name, price
   end
 end
