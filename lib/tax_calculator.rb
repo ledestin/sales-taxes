3d47f7bfd2c6894555c01621ac6243ddc9dde07e
Dmitry Maksyoma <ledestin@gmail.com>
2014-08-15 22:07:11 +1200

    * Refactor.

--- lib/tax_calculator.rb
+++ lib/tax_calculator.rb
@@ -1,6 +1,10 @@
 module TaxCalculator
   def self.calc_tax(amount, tax_rate)
     tax = amount * tax_rate
-    (tax * (1 / 0.05)).ceil / (1 / 0.05)
+    round_up_to_nearest_5_cents(tax)
+  end
+
+  def self.round_up_to_nearest_5_cents amount
+    (amount * (1 / 0.05)).ceil / (1 / 0.05)
   end
 end
