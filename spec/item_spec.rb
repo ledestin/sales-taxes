86bc449b6cdfc0b490e92c7f4581328b59e9c421
Dmitry Maksyoma <ledestin@gmail.com>
2014-08-18 03:40:20 +1200

    * Refactor.

--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "./lib/item"
 
 module FakeTaxRates
-  def self.tax_rate_for_good name
+  def self.tax_rate_for_good(name)
     0.1
   end
 end
@@ -10,37 +10,37 @@ end
 Item = ShoppingCart::Item
 
 describe Item do
-  describe "#tax returns amount of tax for the good:" do
+  describe "#tax returns amount of tax for the item:" do
     it "10% for item" do
-      good = Item.new 1, "item", 10.0, tax_rates: FakeTaxRates
-      expect(good.tax).to eq 1
+      item = Item.new 1, "item", 10, tax_rates: FakeTaxRates
+      expect(item.tax).to eq 1
     end
 
     it "double for 2 items" do
-      good = Item.new 2, "item", 10, tax_rates: FakeTaxRates
-      expect(good.tax).to eq 2
+      item = Item.new 2, "item", 10, tax_rates: FakeTaxRates
+      expect(item.tax).to eq 2
     end
 
     it "rounded up to the nearest 0.05" do
-      good = Item.new 1, "item", 47.30, tax_rates: FakeTaxRates
-      expect(good.tax).to eq 4.75
+      item = Item.new 1, "item", 47.30, tax_rates: FakeTaxRates
+      expect(item.tax).to eq 4.75
     end
   end
 
-  describe "#total returns unit_price + tax, multiplied by quantity" do
+  describe "#total returns total cost of items, including tax:" do
     it "one item" do
-      good = Item.new 1, "item", 10.0, tax_rates: FakeTaxRates
-      expect(good.total).to eq 11
+      item = Item.new 1, "item", 10, tax_rates: FakeTaxRates
+      expect(item.total).to eq 11
     end
 
     it "two items" do
-      good = Item.new 2, "item", 10.0, tax_rates: FakeTaxRates
-      expect(good.total).to eq 22
+      item = Item.new 2, "item", 10.0, tax_rates: FakeTaxRates
+      expect(item.total).to eq 22
     end
   end
 
   it "#to_s returns quantity, name and total" do
-    good = Item.new 2, "item", 10.0, tax_rates: FakeTaxRates
-    expect(good.to_s).to eq "2, item, 22.00"
+    item = Item.new 2, "item", 10, tax_rates: FakeTaxRates
+    expect(item.to_s).to eq "2, item, 22.00"
   end
 end
