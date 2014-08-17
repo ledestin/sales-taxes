dae699dbe57c1c53f30121db1e91f4351c2b3691
Dmitry Maksyoma <ledestin@gmail.com>
2014-08-17 17:28:08 +1200

    * Refactor.

--- lib/receipt.rb
+++ lib/receipt.rb
@@ -1,26 +1,28 @@
 require "./lib/numeric_money_format"
 
-class Receipt
-  using NumericMoneyFormat
+class ShoppingCart
+  class Receipt
+    using NumericMoneyFormat
 
-  def initialize goods
-    @goods = goods
-  end
+    def initialize goods
+      @goods = goods
+    end
 
-  def tax
-    @goods.map(&:tax).inject(:+)
-  end
+    def tax
+      @goods.map(&:tax).inject(:+)
+    end
 
-  def total
-    @goods.map(&:total).inject(:+)
-  end
+    def total
+      @goods.map(&:total).inject(:+)
+    end
 
-  def to_s
-    <<EOF
+    def to_s
+      <<EOF
 #{@goods.join("\n")}
 
 Sales Taxes: #{tax.to_formatted_s(:money)}
 Total: #{total.to_formatted_s(:money)}
 EOF
+    end
   end
 end
--- spec/receipt_spec.rb
+++ spec/receipt_spec.rb
@@ -2,19 +2,19 @@ require "spec_helper"
 require "./lib/item"
 require "./lib/receipt"
 
-describe Receipt do
+describe ShoppingCart::Receipt do
   [:total, :tax].each do |method|
     describe "##{method} returns sum of all goods #{method}" do
       it "for one good" do
 	goods = [ShoppingCart::Item.new(1, "book", 12.49)]
-	expect(Receipt.new(goods).send(method)).to eq \
+	expect(ShoppingCart::Receipt.new(goods).send(method)).to eq \
 	  goods.map(&method).inject(:+)
       end
 
       it "for multiple goods" do
 	goods = [ShoppingCart::Item.new(1, "book", 12.49),
 	  ShoppingCart::Item.new(1, "chocolate bar", 0.85)]
-	expect(Receipt.new(goods).send(method)).to eq \
+	expect(ShoppingCart::Receipt.new(goods).send(method)).to eq \
 	  goods.map(&method).inject(:+)
       end
     end
